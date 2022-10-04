a3f2195d2ae33b176ecbef3445d3af97c9f9dcd6
Ewiggestrige <lixinqing0208@gmail.com>
2022-10-04 11:05:46 +0200

    t

--- bin/test.py
+++ bin/test.py
@@ -16,4 +16,11 @@ class TestCases(unittest.TestCase):
         data_in_json = json.dumps(data, default=lambda obj: obj.__dict__)
         resp = es.index(index="test-index", id="12", document=data_in_json)
 
+    def test03(self):
+        url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=IBM&interval=5min&apikey=9Z5P8Q65555NYU47'
+        r = requests.get(url)
+        data = r.json()
+
+        print(data)
+
 
