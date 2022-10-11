acbdc08829d3036daf1ed6357a3eddc9ac02baf8
LI ZEHAN <lzh1532805942@gmail.com>
2022-10-11 16:40:51 +0800

    Add the sys path

--- core/todayPrice.py
+++ core/todayPrice.py
@@ -1,7 +1,8 @@
-from db.EsConnector import EsConnector
-from entity.DailyPriceOfCompany import DailyPriceOfCompany
 import sys
 sys.path.append('/home/lizehan/project/ElasticSearch')
+from db.EsConnector import EsConnector
+from entity.DailyPriceOfCompany import DailyPriceOfCompany
+
 
 if __name__ == '__main__':
     target_company = {"IBM", "NNND.FRK", "TCEHY", "TCTZF", "0Z4S.LON", "NNN1.FRK", "TME", "63TA.FRK"}
