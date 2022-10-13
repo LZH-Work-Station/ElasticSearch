dbf039195440f10ddc1dd99cde2dec9943840e22
LI ZEHAN <lzh1532805942@gmail.com>
2022-10-13 20:50:18 +0800

    Add roll back

--- bin/rollback_history_data.sh
+++ bin/rollback_history_data.sh
@@ -5,9 +5,8 @@ PARENT_DIR=$(
   cd ..
   pwd
 )
-YESTERDAY=$(date -d "yesterday" +%Y-%m-%d)
 
-if [ $# -eq 2 ]; then
+if [ $# -eq 3 ]; then
   begin_date=$1
   end_date=$2
   while [ "$begin_date" -le "$end_date" ]; do
@@ -15,21 +14,7 @@ if [ $# -eq 2 ]; then
     month=${begin_date:4:2}
     day=${begin_date:6:2}
     begin_date=$(date -d "${begin_date}+1days" +%Y%m%d)
-    python3.6 $PARENT_DIR/core/todayPriceApplication.py $year-$month-$day
-    python3.6 $PARENT_DIR/core/IntraDayPriceApplication.py $year-$month-$day
+    sh $PARENT_DIR/bin/IntraDayPriceApplication $year-$month-$day &
+    sh $PARENT_DIR/bin/todayPriceApplication $year-$month-$day
   done
-fi
-
-if [ $# -eq 1 ]; then
-  date=$1
-  year=${date:0:4}
-  month=${date:4:2}
-  day=${date:6:2}
-  python3.6 "$PARENT_DIR"/core/todayPriceApplication.py $year-$month-$day
-  python3.6 "$PARENT_DIR"/core/IntraDayPriceApplication.py $year-$month-$day
-fi
-
-if [ $# -eq 0 ]; then
-  python3.6 "$PARENT_DIR"/core/todayPriceApplication.py $YESTERDAY
-  python3.6 "$PARENT_DIR"/core/IntraDayPriceApplication.py $YESTERDAY
 fi
