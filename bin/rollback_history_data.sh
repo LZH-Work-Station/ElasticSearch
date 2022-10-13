a9188cf49f029332bd42e79249293bc757130b88
LI ZEHAN <lzh1532805942@gmail.com>
2022-10-13 21:09:08 +0800

    Fix the shell script

--- bin/rollback_history_data.sh
+++ bin/rollback_history_data.sh
@@ -7,15 +7,6 @@ PARENT_DIR=$(
 )
 
 if [ $# -eq 2 ]; then
-  echo "123"
-  begin_date=$1
-  end_date=$2
-  while [ "$begin_date" -le "$end_date" ]; do
-    year=${begin_date:0:4}
-    month=${begin_date:4:2}
-    day=${begin_date:6:2}
-    begin_date=$(date -d "${begin_date}+1days" +%Y%m%d)
-    sh $PARENT_DIR/bin/executor.sh $year-$month-$day $1 $2 todayPriceApplication.py &
-    sh $PARENT_DIR/bin/executor.sh $year-$month-$day $1 $2 intraDayPriceApplication.py
-  done
+  sh $PARENT_DIR/bin/executor.sh $1 $2 todayPriceApplication.py &
+  sh $PARENT_DIR/bin/executor.sh $1 $2 intraDayPriceApplication.py
 fi
