905348fc6472720ef9161040100106287b8e2f4a
LI ZEHAN <lzh1532805942@gmail.com>
2022-10-19 01:25:54 +0800

    Add companys

---
+++ bin/get_company_name.py
@@ -0,0 +1,38 @@
+import os
+import random
+
+current_directory = os.path.dirname(os.path.abspath(__file__))
+from loguru import logger
+
+
+def search_key(file):
+    res = []
+    lines = file.readline()
+    pos = 0
+    count = 0
+    for line in lines.split("("):
+        if pos == 0:
+            pos += 1
+            continue
+        if random.random() <= 0.035:
+            res.append(line.split(")")[0])
+            logger.info(line.split(")")[0] + " with count = " + str(count))
+            count += 1
+
+
+
+
+    file.close()
+    return res
+
+
+def write_key_in_file(file, res):
+    for company in res:
+        file.write(company + '\n')
+    file.close()
+
+
+if __name__ == '__main__':
+    file_input = open(current_directory + '/../conf/initial.txt', mode='r', encoding='utf-8')
+    file_output = open(current_directory + '/../conf/company_symbole.txt', mode='w', encoding='utf-8')
+    write_key_in_file(file_output, search_key(file_input))
--- bin/rollBackDailyPrice.py
+++ bin/rollBackDailyPrice.py
@@ -1,5 +1,5 @@
 import time
-
+from concurrent.futures import ThreadPoolExecutor
 import requests
 from loguru import logger
 from entity.DailyPriceOfCompany import DailyPriceOfCompany
@@ -13,27 +13,31 @@ def get_history_price():
     es = EsConnector()
     rangeDate = RangeDate()
     dates = rangeDate.get_date_iter("2018-12-12", "2022-10-16")
+    pool = ThreadPoolExecutor(max_workers=8)
     for company in target_company:
         url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&outputsize=full&symbol={company}&apikey=B7MFHYK85TIZ85G3".format(
             company=company)
         # 获得该公司的数据
         r = requests.get(url)
         data = r.json()
+
         while 'Note' in data.keys():
             time.sleep(30)
             r = requests.get(url)
             data = r.json()
         # 按照日期进行es写入
         for date in dates:
-            try:
-                daily_company_data = DailyPriceOfCompany.DailyPriceOfCompanyData(data.get("Time Series (Daily)").get(date))
-                daily_data = DailyPriceOfCompany(company, date, daily_company_data)
-                resp = es.index("elk_project_index", daily_data)
-            except Exception as e:
-                logger.warning(
-                    "Can not get the daily price of " + company + " and date = " + date + " with the error: " + str(
-                        data.keys()))
+            pool.submit(insert_into_es, data, es, company, date)
 
+def insert_into_es(data, es, company, date):
+    try:
+        daily_company_data = DailyPriceOfCompany.DailyPriceOfCompanyData(data.get("Time Series (Daily)").get(date))
+        daily_data = DailyPriceOfCompany(company, date, daily_company_data)
+        resp = es.index("elk_project_index", daily_data)
+    except Exception as e:
+        logger.warning(
+            "Can not get the daily price of " + company + " and date = " + date + " with the error: " + str(
+                data.keys()))
 
 
 
