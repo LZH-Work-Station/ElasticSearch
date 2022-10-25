3b244c0c44f698b36b6a4a889d7bdc84d2e9550b
LI ZEHAN <lzh1532805942@gmail.com>
2022-10-25 14:58:46 +0800

    add crontab executor

--- bin/executor.sh
+++ bin/executor.sh
@@ -10,11 +10,11 @@ YESTERDAY=$(date -d "yesterday" +%Y-%m-%d)
 if [ $# -eq 2 ]; then
   begin_date=$1
   end_date=$2
-  python3.6 $PARENT_DIR/bin/rollBackDailyPrice.py $begin_date $end_date;
-  python3.6 $PARENT_DIR/bin/rollBackIntradayPrice.py $begin_date $end_date;
+  python3.6 $PARENT_DIR/bin/rollBackDailyPrice.py $begin_date $end_date
+  python3.6 $PARENT_DIR/bin/rollBackIntradayPrice.py $begin_date $end_date
 fi
 
 if [ $# -eq 0 ]; then
-  python3.6 $PARENT_DIR/bin/rollBackDailyPrice.py $YESTERDAY $YESTERDAY;
-  python3.6 $PARENT_DIR/bin/rollBackIntradayPrice.py $YESTERDAY $YESTERDAY;
+  python3.6 $PARENT_DIR/bin/rollBackDailyPrice.py $YESTERDAY $YESTERDAY
+  python3.6 $PARENT_DIR/bin/rollBackIntradayPrice.py $YESTERDAY $YESTERDAY
 fi
